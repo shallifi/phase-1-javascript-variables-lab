ad244642cb9900609facd05423a999b35c70a9d7
Benjamin <shallifi@hotmail.com>
2022-03-22 23:23:20 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+console.log(companyName);
+
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+console.log(mostProfitableNeighborhood);
+
+let companyCeo = 'Susan Smith';
+console.log(companyCeo);
